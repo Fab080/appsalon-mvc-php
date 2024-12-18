47c135810bc03860dc79912fe45b2411213651c0
RumbosExpress <rumbosexpress@gmail.com>
2024-12-18 14:24:19 -0500

    Primer commit

---
+++ views/auth/confirmar-cuenta.php
@@ -0,0 +1,9 @@
+<h1 class="nombre-pagina">Confirmar Cuenta</h1>
+
+<?php
+include_once __DIR__ . "/../templates/alertas.php";
+?>
+
+<div class="acciones">
+	<a href="/">Ya tienes una cuenta. Iniciar Sesión.</a>
+</div>
